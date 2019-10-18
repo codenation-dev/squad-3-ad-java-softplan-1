9a1d6722df0a1aa3a3bae7439125d79306dc1023
Victor V. Paulo <victorvpaulo@gmail.com>
2019-10-18 03:27:14 -0300

    Change project structure.

--- errorlogger/src/main/java/br/com/aceleradev/javasquad3/errorlogger/repository/ErrorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.aceleradev.javasquad3.errorlogger.repository;
-
-import br.com.aceleradev.javasquad3.errorlogger.model.Error;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ErrorRepository extends JpaRepository<Error, Long> {
-}
