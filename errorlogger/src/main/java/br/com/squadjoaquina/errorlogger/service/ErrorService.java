22f55238d5fe0aaf36d1f1c971589e5655c07732
P Mukuna <pcishiku@gmail.com>
2019-10-28 22:50:37 -0300

    Update ErrorService.java

--- errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/service/ErrorService.java
+++ errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/service/ErrorService.java
@@ -6,6 +6,8 @@ import br.com.squadjoaquina.errorlogger.repository.ErrorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+
 @Service
 public class ErrorService {
 
@@ -17,7 +19,19 @@ public class ErrorService {
     }
 
     public void saveError(ErrorDTO errorDTO){
+        errorDTO.setCreateAt(new Timestamp(System.currentTimeMillis()));
         errorRepository.save(ErrorMapper.toError(errorDTO));
     }
 
+    public ErrorDTO findError(Long id){
+        return ErrorMapper.toErrorDTO(errorRepository.findErrorById(id));
+    }
+
+    public void stashError(Long id) {
+        ErrorDTO error = findError(id);
+        error.setArchived(true);
+        error.setArchivedDate(new Timestamp(System.currentTimeMillis()));
+
+        errorRepository.save(ErrorMapper.toError(error));
+    }
 }
