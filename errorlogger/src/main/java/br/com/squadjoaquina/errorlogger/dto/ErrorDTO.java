798438352ccb366c8dba181aa746fdc51c3ce2ee
pacifiquecishiku <pcishiku@gmail.com>
2019-10-31 23:23:43 -0300

    Endpoint para arquivar erro implemetado

--- errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/dto/ErrorDTO.java
+++ errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/dto/ErrorDTO.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.sql.Timestamp;
 
 @Getter @Setter @NoArgsConstructor
