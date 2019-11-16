51b2deb0974cd2d65cab7272991012481233f764
Victor V. Paulo <victorvpaulo@gmail.com>
2019-11-16 07:53:48 -0300

    Change of search endpoint to /error/aggregates

--- errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/controller/ErrorController.java
+++ errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/controller/ErrorController.java
@@ -63,7 +63,7 @@ public class ErrorController {
                     message = "Você não possui autorização para utilizar este" +
                               " recurso.")
     })
-    @GetMapping(produces = "application/json")
+    @GetMapping(value = "/aggregates",produces = "application/json")
     public ResponseEntity<Page<ErrorAggregate>> search(
             @RequestParam(value = "environment", required = false) Environment environment,
             @RequestParam(value = "level", required = false) Level level,
