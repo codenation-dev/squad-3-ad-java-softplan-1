78ebc66dd6e1434d80bafed5bad182fe7e85981a
Victor V. Paulo <victorvpaulo@gmail.com>
2019-11-16 23:20:15 -0300

    Fix code to remove SonarLint warnings.

--- errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/swagger/SwaggerConfig.java
+++ errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/swagger/SwaggerConfig.java
@@ -2,8 +2,6 @@ package br.com.squadjoaquina.errorlogger.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,26 +17,25 @@ public class SwaggerConfig {
     @Bean
     public Docket apiDocket() {
 
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(
                         "br.com.squadjoaquina.errorlogger"))
                 .paths(PathSelectors.any())
-                .build().apiInfo(metaData()).useDefaultResponseMessages(false);
-
-        return docket;
+                .build()
+                .apiInfo(metaData())
+                .useDefaultResponseMessages(false);
     }
 
     private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo("ErrorLogger REST API",
-                                      "API REST para registro  e " +
-                                      "monitoramento de erros.",
-                                      "1.0",
-                                      "",
-                                      "",
-                                      "",
-                                      "");
-        return apiInfo;
+        return new ApiInfo("ErrorLogger REST API",
+                           "API REST para registro  e " +
+                           "monitoramento de erros.",
+                           "1.0",
+                           "",
+                           "",
+                           "",
+                           "");
     }
 
 }
