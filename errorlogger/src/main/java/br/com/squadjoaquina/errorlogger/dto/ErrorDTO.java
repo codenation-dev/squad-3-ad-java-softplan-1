97b79049189ee7c8e03e894ef8252b152d22012d
Victor V. Paulo <victorvpaulo@gmail.com>
2019-11-18 16:57:56 -0300

    Improve ErrorDTO json return.

--- errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/dto/ErrorDTO.java
+++ errorlogger/src/main/java/br/com/squadjoaquina/errorlogger/dto/ErrorDTO.java
@@ -2,6 +2,7 @@ package br.com.squadjoaquina.errorlogger.dto;
 
 import br.com.squadjoaquina.errorlogger.model.Environment;
 import br.com.squadjoaquina.errorlogger.model.Level;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -21,13 +22,14 @@ public class ErrorDTO {
     @ApiModelProperty(notes = "Id do erro gerado pela base de dados.")
     private Long id;
 
-    @ApiModelProperty(notes = "Aplicação que deu origem ao erro.")
+    @ApiModelProperty(notes = "Aplicação que deu origem ao erro.", required =
+            true)
     @NotBlank
     private String origin;
 
     @ApiModelProperty(notes = "Título do erro.\n" +
                               "Recomenda-se a utilização do nome " +
-                              "da Exception (ou equivalente).")
+                              "da Exception (ou equivalente).", required = true)
     @NotBlank
     private String title;
 
@@ -36,21 +38,25 @@ public class ErrorDTO {
                               "(ou equivalente).")
     private String description;
 
-    @ApiModelProperty(notes = "Id do User que registrou o erro.")
+    @ApiModelProperty(notes = "Id do User que registrou o erro.",
+            required = true)
     @NotNull
     private Long userID;
 
-    @ApiModelProperty(notes = "Level de severidade do erro")
+    @ApiModelProperty(notes = "Level de severidade do erro", required = true)
     @NotNull
     private Level level;
 
-    @ApiModelProperty(notes = "Ambiente no qual ocorreu o erro.")
+    @ApiModelProperty(notes = "Ambiente no qual ocorreu o erro.",
+            required = true)
     @NotNull
     private Environment environment;
 
     @ApiModelProperty(notes = "Data de arquivação do erro.")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date archivedAt;
 
     @ApiModelProperty(notes = "Data de criação do erro. Imutável.")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Date createdAt;
 }
