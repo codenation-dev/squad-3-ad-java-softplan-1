c11943deb1d74144e9e7e1063cf91bb840055ebf
Andi <andersonmgaspar@gmail.com>
2019-11-29 14:58:19 -0300

    Novo Exception Handler para Email não encontrado

--- src/main/java/br/com/squadjoaquina/errorlogger/controller/advice/ResponseExceptionHandler.java
+++ src/main/java/br/com/squadjoaquina/errorlogger/controller/advice/ResponseExceptionHandler.java
@@ -2,13 +2,14 @@ package br.com.squadjoaquina.errorlogger.controller.advice;
 
 import br.com.squadjoaquina.errorlogger.service.exception.ErrorAlreadyArchivedException;
 import br.com.squadjoaquina.errorlogger.service.exception.ErrorNotFoundException;
+import br.com.squadjoaquina.errorlogger.service.exception.UserEmailNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class ErrorControllerExceptionHandler {
+public class ResponseExceptionHandler {
 
     @ExceptionHandler(value = ErrorNotFoundException.class)
     ResponseEntity<Void> handleErrorNotFound() {
@@ -20,6 +21,13 @@ public class ErrorControllerExceptionHandler {
     ResponseEntity<Void> handleErrorNotFound(
             ErrorAlreadyArchivedException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT)
+                             .build();
+    }
+
+    @ExceptionHandler(value = UserEmailNotFoundException.class)
+    ResponseEntity<Void> handleUserEmailNotFound(
+            UserEmailNotFoundException e) {
+        return ResponseEntity.notFound()
                              .build();
     }
 
